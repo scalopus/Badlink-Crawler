cbb451b700b303c3e2d514263e139672a5aaf095
WarunK <WarunK@6b775633-4830-fb42-a769-f649db716bb6>
2011-05-05 01:25:12 +0000

    Yii Framework Template
    git-svn-id: svn://localhost/badlink/trunk@4 6b775633-4830-fb42-a769-f649db716bb6

---
+++ cli-tools/protected/config/console.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+	'name'=>'Crawler Console',
+        'import' => array('application.components.*'),
+
+        );
+
+
